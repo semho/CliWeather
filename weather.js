dd4881289b97a40fa6294bf51971d48ce84b8e21
unknown <semho@yandex.ru>
2022-12-06 07:48:00 +0300

    error handling

--- weather.js
+++ weather.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { getArgs } from "./helpers/args.js";
+import { getWeather } from "./services/api.service.js";
 import { printHelp, printSuccess, printError } from "./services/log.service.js";
 import { saveKeyValue, TOKEN_DICTIONARY } from "./services/storage.service.js";
 
@@ -16,6 +17,21 @@ const saveToken = async (token) => {
   }
 };
 
+const getForecast = async () => {
+  try {
+    const weather = await getWeather("yaroslavl");
+    console.log(weather);
+  } catch (error) {
+    if (error?.response?.status == 404) {
+      printError("Не верно указан город");
+    } else if (error?.response?.status == 401) {
+      printError("Не верно указан токен");
+    } else {
+      printError(error.message);
+    }
+  }
+};
+
 const initCli = () => {
   const args = getArgs(process.argv);
   if (args.h) {
@@ -29,6 +45,7 @@ const initCli = () => {
   }
 
   //вывод погоды
+  getForecast();
 };
 
 initCli();
