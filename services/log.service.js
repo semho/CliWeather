f9a1a84c8733444964528129a09871527b08e20b
unknown <semho@yandex.ru>
2022-12-06 07:31:26 +0300

    added api in service

--- services/log.service.js
+++ services/log.service.js
@@ -2,16 +2,16 @@ import chalk from "chalk";
 import dedent from "dedent-js";
 
 const printError = (error) => {
-  console.log(chalk.bgRed("ERROR") + " " + error);
+  console.log(chalk.bgRed(" ERROR ") + " " + error);
 };
 
 const printSuccess = (message) => {
-  console.log(chalk.bgGreen("SUCCESS") + " " + message);
+  console.log(chalk.bgGreen(" SUCCESS ") + " " + message);
 };
 
 const printHelp = () => {
   console.log(
-    dedent`${chalk.bgCyan("HELP")}
+    dedent`${chalk.bgCyan(" HELP ")}
         Без параметров - вывод погоды
         -s [CITY] для установки города
         -h для вывода помощи
--- weather.js
+++ weather.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 import { getArgs } from "./helpers/args.js";
 import { printHelp, printSuccess, printError } from "./services/log.service.js";
-import { saveKeyValue } from "./services/storage.service.js";
+import { saveKeyValue, TOKEN_DICTIONARY } from "./services/storage.service.js";
 
 const saveToken = async (token) => {
+  if (!token.length) {
+    printError("Не передан токен");
+    return;
+  }
   try {
-    await saveKeyValue("token", token);
+    await saveKeyValue(TOKEN_DICTIONARY.token, token);
     printSuccess("Токен сохранен");
   } catch (error) {
     printError(error.message);
