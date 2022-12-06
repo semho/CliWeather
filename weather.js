81693f4c8b0939b1da90ace7deda0bc23d0385da
unknown <semho@yandex.ru>
2022-12-06 07:01:19 +0300

    added storage in service

--- weather.js
+++ weather.js
@@ -1,7 +1,16 @@
 #!/usr/bin/env node
 import { getArgs } from "./helpers/args.js";
-import { printHelp } from "./services/log.service.js";
+import { printHelp, printSuccess, printError } from "./services/log.service.js";
 import { saveKeyValue } from "./services/storage.service.js";
+
+const saveToken = async (token) => {
+  try {
+    await saveKeyValue("token", token);
+    printSuccess("Токен сохранен");
+  } catch (error) {
+    printError(error.message);
+  }
+};
 
 const initCli = () => {
   const args = getArgs(process.argv);
@@ -12,7 +21,7 @@ const initCli = () => {
     //сохраняем город
   }
   if (args.t) {
-    saveKeyValue("token", args.t);
+    return saveToken(args.t);
   }
 
   //вывод погоды
